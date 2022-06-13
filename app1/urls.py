cdbcf75d0355c0e02a1451c6e59199da72bd3d75
mukundmurali369 <mukundpolicodu@gmail.com>
2022-06-13 16:50:09 +0530

    second commit

--- app1/urls.py
+++ app1/urls.py
@@ -4,4 +4,6 @@ from .import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('load_create_groups',views.load_create_groups,name='load_create_groups'),
+    path('load_create_ledgers',views.load_create_ledgers,name='load_create_ledgers'),
 ]
--- app1/views.py
+++ app1/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render,redirect
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def load_create_groups(request):
+    return render(request,'load_create_groups.html')
+    
+def load_create_ledgers(request):
+    return render(request,'load_create_ledgers.html')
