0d5385d13df03b1a9786d4a35ef568be3be21fe7
mukundmurali369 <mukundpolicodu@gmail.com>
2022-06-14 16:49:16 +0530

    third commit

--- app1/urls.py
+++ app1/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('load_create_groups',views.load_create_groups,name='load_create_groups'),
     path('load_create_ledgers',views.load_create_ledgers,name='load_create_ledgers'),
+    path('load_create_select_currency',views.load_create_select_currency,name='load_create_select_currency'),
+    path('load_create_currency',views.load_create_currency,name='load_create_currency'),
+    path('load_alter_currency',views.load_alter_currency,name='load_alter_currency'),
 ]
--- app1/views.py
+++ app1/views.py
@@ -3,10 +3,19 @@ from django.shortcuts import render,redirect
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'base.html')
 
 def load_create_groups(request):
     return render(request,'load_create_groups.html')
     
 def load_create_ledgers(request):
     return render(request,'load_create_ledgers.html')
+
+def load_create_select_currency(request):
+    return render(request,'load_create_select_currency.html')
+
+def load_create_currency(request):
+    return render(request,'load_create_currency.html')
+
+def load_alter_currency(request):
+    return render(request,'load_alter_currency.html')
