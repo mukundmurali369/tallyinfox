4026e59a5cc5b1a9dd9c4181a1e073099f5afcd9
mukundmurali369 <mukundpolicodu@gmail.com>
2022-06-16 16:47:22 +0530

    fifth commit

--- app1/urls.py
+++ app1/urls.py
@@ -5,12 +5,16 @@ from .import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('load_create_groups',views.load_create_groups,name='load_create_groups'),
-    path('load_create_ledgers',views.load_create_ledgers,name='load_create_ledgers'),
+    path('load_create_ledger',views.load_create_ledger,name='load_create_ledger'),
     path('load_create_select_currency',views.load_create_select_currency,name='load_create_select_currency'),
     path('load_create_currency',views.load_create_currency,name='load_create_currency'),
     path('load_alter_currency',views.load_alter_currency,name='load_alter_currency'),
     path('load_create_vouchertyp',views.load_create_vouchertyp,name='load_create_vouchertyp'),
+
+    
+    path('load_create_employee',views.load_create_employee,name='load_create_employee'), 
     path('load_create_empcat',views.load_create_empcat,name='load_create_empcat'), 
     path('load_create_empgrp',views.load_create_empgrp,name='load_create_empgrp'),
     path('load_create_empatnd',views.load_create_empatnd,name='load_create_empatnd'),  
+    path('load_create_unit',views.load_create_unit,name='load_create_unit'),  
 ]
--- app1/views.py
+++ app1/views.py
@@ -8,8 +8,8 @@ def index(request):
 def load_create_groups(request):
     return render(request,'load_create_groups.html')
     
-def load_create_ledgers(request):
-    return render(request,'load_create_ledgers.html')
+def load_create_ledger(request):
+    return render(request,'load_create_ledger.html')
 
 def load_create_select_currency(request):
     return render(request,'load_create_select_currency.html')
@@ -22,6 +22,9 @@ def load_alter_currency(request):
 
 def load_create_vouchertyp(request):
     return render(request,'load_create_vouchertyp.html')
+  
+def load_create_employee(request):
+    return render(request,'load_create_employee.html')
 
 def load_create_empcat(request):
     return render(request,'load_create_empcat.html')
@@ -31,3 +34,58 @@ def load_create_empgrp(request):
     
 def load_create_empatnd(request):
     return render(request,'load_create_empatnd.html')
+
+def load_create_unit(request):
+    return render(request,'load_create_unit.html')
+
+
+
+def create_ledger(request):
+    if request.method == 'POST':
+        print('xxxxxxxxxxxxxxxxxxx')
+        # Ledger Basic
+        Lname = request.POST['Lname']
+        Lalias = request.POST['Lalias']
+        Lunder = request.POST['Lund']
+        Lopening_bal = request.POST['Lopening']
+        # Banking_details
+        B_od_limit = request.POST['B_od_limit']
+        B_ac_holder_name = request.POST['B_ac_name']
+        B_ac_no = request.POST['B_ac_no']
+        B_ifsc = request.POST['B_ac_ifsc']
+        B_swift_code = request.POST['B_ac_swift']
+        B_name = request.POST['B_name']
+        B_branch = request.POST['B_branch']
+        '''bank Configuration'''
+        B_alter_chq_bks = request.POST['B_alter_chq_bks']  # bool
+        B_name_enbl_chq_prtg = request.POST['B_name_enbl_chq_prtg']  # bool
+
+        # Mailing_details
+        Mname = request.POST['Mname']
+        Maddress = request.POST['Maddress']
+        Mstate = request.POST['Mstate']
+        Mcountry = request.POST['Mcountry']
+        Mpincode = request.POST['Mpincode']
+        # Provide Banking Details
+        provide_banking = request.POST['provide_banking']  # bool
+
+        # Tax_Registration_Details
+        Tgst_uin = request.POST['Tgst_uin']
+        Treg_typ = request.POST['Treg_typ']
+        Tpan_no = request.POST['Tpan_no']
+        T_alter_gst = request.POST['T_alter_gst']
+
+        print(
+            Lunder,
+            Mname,
+            Maddress,
+            provide_banking,
+            Tpan_no
+
+        )
+
+    grp_under_lst = GroupModel.objects.all().order_by('name')
+    context = {
+        'grp': grp_under_lst,
+    }
+    return render(request, 'load_create_ledgers.html', context)
