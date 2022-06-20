fd0a6a12eec5b5c64041750dc7404073669b612e
mukundmurali369 <mukundpolicodu@gmail.com>
2022-06-20 16:47:09 +0530

    create currency and emp category completed

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+from unicodedata import decimal
 from django.db import models
 
 # Create your models here.
@@ -13,3 +15,40 @@ class GroupModel(models.Model):
     def __str__(self):
         return self.name
     
+class CreateCurrency(models.Model):
+    symbol =models.CharField(max_length=225)
+    formal_name=models.CharField(max_length=225)
+    ISO_code=models.CharField(max_length=225)
+    decimal_places= models.CharField(max_length=225,default=2)
+    show_in_millions =  models.CharField(max_length=225)
+    suffix_to_amount=  models.CharField(max_length=225)
+    space_symbol_amount = models.CharField(max_length=225)
+    word_after_decimal = models.CharField(max_length=225)
+    decimal_no_in_words = models.CharField(max_length=225)
+
+class CreateEmployeeCategory(models.Model):
+    name =models.CharField(max_length=225)
+    alias=models.CharField(max_length=225)
+    allocate_revenue=models.CharField(max_length=225)
+    allocate_nonrevenue=models.CharField(max_length=225)
+
+
+
+class VoucherModel(models.Model):
+    voucher_name = models.CharField(max_length=225)
+    alias = models.CharField(max_length=225)
+    voucher_type = models.CharField(max_length=225)
+    abbreviation = models.CharField(max_length=225)
+    active_this_voucher_type = models.BooleanField()
+    method_voucher_numbering = models.CharField(max_length=225)
+    use_effective_date = models.BooleanField()
+    allow_zero_value_trns = models.BooleanField()
+    allow_naration_in_voucher = models.BooleanField()
+    enable_default_ac_allocation = models.BooleanField()
+    track_additional_cost_purchase = models.BooleanField()
+    use_as_manf_journal = models.BooleanField()
+    print_voucher_af_save = models.BooleanField()
+    print_formal_recept = models.BooleanField()
+    default_juridiction = models.CharField(max_length=225)
+    default_title = models.CharField(max_length=225)
+    alter_decalaration = models.BooleanField()
--- app1/views.py
+++ app1/views.py
@@ -51,6 +51,11 @@ def load_credit_list(request):
 def load_multi_ledger_alter(request):
     return render(request,'load_multi_ledger_alter.html')
 
+def load_rates_of_exchange(request):
+    obj=CreateCurrency.objects.all()
+    context={'grp': obj}
+    return render(request,'load_rates_of_exchange.html',context)
+
 
 
 
@@ -89,3 +94,113 @@ def create_group(request):
         return JsonResponse({
             'status': 1
         })
+
+
+def create_currency(request):
+    if request.method == 'POST':
+        symbol = request.POST['symbol']
+        fname = request.POST['fname']
+        if len(symbol) <= 0:
+            print('XX')
+            return JsonResponse({
+                'status': 00
+            })
+        elif len(fname) <= 0:
+            print('XXX')
+            return JsonResponse({
+                'status': 00
+            })
+        else:
+            pass
+
+        iso_code = request.POST['iso_code']
+        n_deci_placs = request.POST['n_deci_placs']
+        smt_millon = request.POST['smt_millon']
+        symbol_to_amount = request.POST['symbol_to_amount']
+        space_bt_sy = request.POST['space_bt_sy']
+        amount_after_decimal = request.POST['amount_after_decimal']
+        amount_in_words = request.POST['amount_in_words']
+
+        mdl_obj = CreateCurrency(
+            symbol=symbol,
+            formal_name=fname,
+            ISO_code=iso_code,
+            decimal_places=n_deci_placs,
+            show_in_millions=smt_millon,
+            suffix_to_amount=symbol_to_amount,
+            space_symbol_amount=space_bt_sy,
+            word_after_decimal=amount_after_decimal,
+            decimal_no_in_words=amount_in_words,
+        )
+        mdl_obj.save()
+        return redirect('load_create_currency')
+
+
+def save_empcat(request):
+    if request.method == 'POST':
+        namee = request.POST['name']
+        aliass = request.POST['alias']
+        rev = request.POST['allocaterev']
+        nonrev = request.POST['allocatenonrev']
+        
+        mdl_obj = CreateEmployeeCategory(
+            name =namee,
+            alias=aliass,
+            allocate_revenue=rev,
+            allocate_nonrevenue=nonrev,
+           
+        )
+        mdl_obj.save()
+        return redirect('load_create_empcat')
+
+
+
+def create_voucher(request):
+    if request.method == 'POST':
+        Vname = request.POST['nam']
+        alias = request.POST['alias']
+        vtype = request.POST['vtype']
+        abbre = request.POST['abbre']
+        activ_vou_typ = request.POST['avtyp']  # bool
+        meth_vou_num = request.POST['meth_vou_num']
+        use_effct_date = request.POST['uefftdate']  # bool
+        allow_zero_trans = request.POST['allow_zero_trans']  # bool
+        allow_naration_in_vou = request.POST['allow_naration_in_vou']  # bool
+        enbl_def_accout_alloc = request.POST['enbl_def_accout_alloc']  # bool
+        track_addi_coast = request.POST['track_addi_coast']  # bool
+        # bool
+        use_as_manfacturing_journal = request.POST['use_as_manfacturing_journal']
+        #
+        print_vou_aft_save = request.POST['print_vou_aft_save']  # b
+        print_formal_recept = request.POST['print_formal_recept']  # b
+        def_juri = request.POST['def_juri']
+        default_title = request.POST['default_title']
+        alte_declaration = request.POST['alte_declaration']  # b
+
+        mdl = VoucherModel(
+            voucher_name=Vname,
+            alias=alias,
+            voucher_type=vtype,
+            abbreviation=abbre,
+            active_this_voucher_type=activ_vou_typ,
+            method_voucher_numbering=meth_vou_num,
+            use_effective_date=use_effct_date,
+            allow_zero_value_trns=allow_zero_trans,
+            allow_naration_in_voucher=allow_naration_in_vou,
+            enable_default_ac_allocation=enbl_def_accout_alloc,
+            track_additional_cost_purchase=track_addi_coast,
+            use_as_manf_journal=use_as_manfacturing_journal,
+            print_voucher_af_save=print_vou_aft_save,
+            print_formal_recept=print_formal_recept,
+            default_juridiction=def_juri,
+            default_title=default_title,
+            alter_decalaration=alte_declaration,
+
+        )
+        mdl.save()
+        return redirect('base.html')
+
+    context = {
+
+    }
+    return render(request, 'load_create_vouchertyp', context)
