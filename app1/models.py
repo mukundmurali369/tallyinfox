11fee5ad0a7cde84d9fe32a12f98b2b8908e9431
mukundmurali369 <mukundpolicodu@gmail.com>
2022-06-22 16:51:13 +0530

    nineth commit employee reg complete

--- app1/models.py
+++ app1/models.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+import email
 from locale import currency
 from unicodedata import decimal
 from django.db import models
@@ -77,3 +78,88 @@ class CurrencyAlter(models.Model):
     specirate =models.CharField(max_length=225)
     lastvrate2 =models.CharField(max_length=225)
     specirate2 =models.CharField(max_length=225)
+
+class Employee(models.Model):
+
+    name = models.CharField(max_length=225)
+    alias= models.CharField(max_length=225)
+    under= models.CharField(max_length=225)
+    date_join = models.DateField()
+    defn_sal = models.CharField(max_length=225)
+    emp_name = models.CharField(max_length=225)
+    emp_desg = models.CharField(max_length=225)
+    fnctn = models.CharField(max_length=225)
+    location = models.CharField(max_length=225)
+    gender= models.CharField(max_length=225)
+    dob = models.DateField()
+    blood = models.CharField(max_length=225)
+    parent_name =models.CharField(max_length=225)
+    spouse_name =models.CharField(max_length=225)
+    address =models.CharField(max_length=225)
+    number =models.CharField(max_length=225)
+    emailid =models.CharField(max_length=225)
+    inc_tax_no =models.CharField(max_length=225)
+    aadhar_no=models.CharField(max_length=225)
+    uan =models.CharField(max_length=225)
+    pfn =models.CharField(max_length=225)
+    pran =models.CharField(max_length=225)
+    esin =models.CharField(max_length=225)
+    bankdtls=models.CharField(max_length=225)
+
+
+
+class LedgerModel(models.Model):
+  
+    ledger_name = models.CharField(max_length=225)
+    ledger_alias = models.CharField(max_length=225)
+
+
+    ledger_opening_bal = models.CharField(max_length=225)
+    ledger_type = models.CharField(max_length=225)
+    type_of_duty = models.CharField(max_length=225)
+    percent_of_calculation = models.CharField(max_length=225)
+    maintain_bal_bill = models.CharField(max_length=225)
+    credit_days_during_voucher_entry = models.CharField(max_length=225)
+    default_cr_peroid = models.CharField(max_length=225)
+    provide_banking_details = models.BooleanField()
+
+    def __str__(self):
+        return self.ledger_name
+
+
+class BankingDetails(models.Model):
+   
+    od_limit = models.CharField(max_length=225)
+    holder_name = models.CharField(max_length=225)
+    ac_number = models.CharField(max_length=225)
+    ifsc = models.CharField(max_length=225)
+    swift_code = models.CharField(max_length=225)
+    bank_name = models.CharField(max_length=225)
+    branch_name = models.CharField(max_length=225)
+    alter_chk_bks = models.BooleanField()
+    enbl_chk_printing = models.BooleanField()
+
+
+class MailingAddressModel(models.Model):
+  
+    name = models.CharField(max_length=225)
+    address = models.CharField(max_length=225)
+    state = models.CharField(max_length=225)
+    country = models.CharField(max_length=225)
+    pincode = models.CharField(max_length=225)
+
+
+class TaxRegisterModel(models.Model):
+    
+    gst_uin = models.CharField(max_length=225)
+    register_type = models.CharField(max_length=225)
+    pan_no = models.CharField(max_length=225)
+    alter_gst_details = models.BooleanField()
+
+
+class LedgerSatutoryModel(models.Model):
+ 
+    assessable_calculation = models.CharField(max_length=225)
+    gst_applicable = models.CharField(max_length=225)
+    type_of_supply = models.CharField(max_length=225)
+
